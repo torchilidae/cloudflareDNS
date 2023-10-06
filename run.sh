c096bf97fb697c9656fe336fc12f40dfb74d7bf5
siva1024 <siva.radium@gmail.com>
2023-10-05 19:30:09 -0700

    Test for curl commands

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-echo "Hello, world!"
+#!/bin/bash
+
+# Your Cloudflare API credentials
+ACCOUNT_ID=${CF_ACCOUNT_ID}
+ZONE_ID=${CF_ZONE_ID}
+API_KEY=${CF_TOKEN}
+
+curl 'https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A' --header 'Authorization:  Bearer ${CF_TOKEN}'
