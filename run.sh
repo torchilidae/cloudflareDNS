9393c2bb035dad42c41892c5609940d1c797b7a6
siva1024 <siva.radium@gmail.com>
2023-10-05 19:35:49 -0700

    Test secrets

--- run.sh
+++ run.sh
@@ -5,4 +5,7 @@ ACCOUNT_ID=${CF_ACCOUNT_ID}
 ZONE_ID=${CF_ZONE_ID}
 API_KEY=${CF_TOKEN}
 
+echo ${CF_ZONE_ID}
+echo $CF_ZONE_ID
+
 curl 'https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A' --header 'Authorization:  Bearer ${CF_TOKEN}'
