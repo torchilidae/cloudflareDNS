66961ad454d3ee03ae1777652a9dc9fc86eb6e57
siva1024 <siva.radium@gmail.com>
2023-10-05 19:36:49 -0700

    Updated quotes

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ API_KEY=${CF_TOKEN}
 echo ${CF_ZONE_ID}
 echo $CF_ZONE_ID
 
-curl 'https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A' --header 'Authorization:  Bearer ${CF_TOKEN}'
+curl "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A" --header "Authorization:  Bearer ${CF_TOKEN}"
