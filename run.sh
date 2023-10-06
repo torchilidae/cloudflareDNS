4bdba9374389b04cf36fc518e27752d60ae29d56
siva1024 <siva.radium@gmail.com>
2023-10-06 05:25:27 -0700

    Updated to multi DNS

--- run.sh
+++ run.sh
@@ -7,31 +7,25 @@ API_KEY=${CF_TOKEN}
 # URL for Cloudflare API
 API_URL="https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records"
 
-# Function to update or create DNS record
-update_or_create_dns_record() {
+# Function to update or create DNS records for a hostname
+update_or_create_dns_records() {
     local hostname="$1"
-    local new_ip="$2"
+    local new_ips="$2"
 
-    # Find the DNS record by hostname
+    # Find the DNS records by hostname
     response=$(curl -s -X GET "${API_URL}?name=${hostname}&type=A" \
         -H "Authorization: Bearer ${CF_TOKEN}")
 
-    record_id=$(echo "$response" | jq -r '.result[0].id')
+    record_ids=$(echo "$response" | jq -r '.result[].id')
 
-    if [ -n "$record_id" ]; then
-        # Update the existing DNS record with the new IP
-        update_response=$(curl -s -X PUT "${API_URL}/${record_id}" \
-            -H "Authorization: Bearer ${CF_TOKEN}" \
-            -H "Content-Type: application/json" \
-            --data "{\"type\":\"A\",\"name\":\"${hostname}\",\"content\":\"${new_ip}\"}")
+    # Delete existing DNS records
+    for record_id in $record_ids; do
+        delete_response=$(curl -s -X DELETE "${API_URL}/${record_id}" \
+            -H "Authorization: Bearer ${CF_TOKEN}")
+    done
 
-        if [ "$(echo "$update_response" | jq -r '.success')" == "true" ]; then
-            echo "Successfully updated ${hostname} to ${new_ip}"
-        else
-            echo "Failed to update ${hostname}: $(echo "$update_response" | jq -r '.errors[0].message')"
-        fi
-    else
-        # Create a new DNS record
+    # Create new DNS records with the new IPs
+    for new_ip in ${new_ips[@]}; do
         create_response=$(curl -s -X POST "${API_URL}" \
             -H "Authorization: Bearer ${CF_TOKEN}" \
             -H "Content-Type: application/json" \
@@ -40,9 +34,9 @@ update_or_create_dns_record() {
         if [ "$(echo "$create_response" | jq -r '.success')" == "true" ]; then
             echo "Successfully created ${hostname} with IP ${new_ip}"
         else
-            echo "Failed to create ${hostname}: $(echo "$create_response" | jq -r '.errors[0].message')"
+            echo "Failed to create ${hostname} with IP ${new_ip}: $(echo "$create_response" | jq -r '.errors[0].message')"
         fi
-    fi
+    done
 }
 
 # Load data from JSON file
@@ -53,8 +47,8 @@ if [ -f "$input_file" ]; then
     # Iterate through records and update or create them
     for row in $(echo "${data}" | jq -c '.records[]'); do
         hostname=$(echo "$row" | jq -r '.hostname')
-        new_ip=$(echo "$row" | jq -r '.new_ip')
-        update_or_create_dns_record "$hostname" "$new_ip"
+        new_ips=$(echo "$row" | jq -r '.new_ips[]')
+        update_or_create_dns_records "$hostname" "$new_ips"
     done
 else
     echo "Input JSON file not found: ${input_file}"
